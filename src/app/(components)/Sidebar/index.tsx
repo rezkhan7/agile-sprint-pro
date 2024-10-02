5fe736f169e21b7bb4df03cac9f1bb2e0bdc7c51
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2024-10-02 13:30:27 -0400

    Testing

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
             </div>
             {/* Team */}
             <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700'>
-            <Image src="/logo.png" alt='Logo' width={40} height={40} />
+                <Image src="/logo.png" alt='Logo' width={40} height={40} /> 
             <div>
                 <h3 className='text-md font-bold tracking-wide dark:text-gray-200'> EDROH TEAM </h3>
                 <div className='mt-1 flex items-start gap-2'>
--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -5,18 +5,17 @@ import Navbar from '@/app/(components)/Navbar';
 import Sidebar from '@/app/(components)/Sidebar';
 import StoreProvider, { useAppSelector } from './redux';
 
-
 const DashboardLayout = ({ children }: { children: React.ReactNode}) =>  {
-  const isSidebarCollapsed = useAppSelector((state)=> state.global.isSidebarCollapsed)
-  const isDarkMode = useAppSelector((state)=> state.global.isDarkMode)
+    const isSidebarCollapsed = useAppSelector((state)=> state.global.isSidebarCollapsed)
+    const isDarkMode = useAppSelector((state)=> state.global.isDarkMode)
   
-  useEffect(()=>{
+useEffect(()=>{
     if(isDarkMode)
         document.documentElement.classList.add('dark')
     else
         document.documentElement.classList.remove('dark') 
     
-  })
+  }, [isDarkMode])
 
   return (
     <div className=' flex min-h-screen w-full bg-gray-50 text-gray-900'>
@@ -36,9 +35,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode}) =>  {
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode}) =>{
     return(
-        <StoreProvider>
-            <DashboardLayout> {children} </DashboardLayout>
-        </StoreProvider>
+        <StoreProvider> 
+            <DashboardLayout> </DashboardLayout>
+        </StoreProvider> 
     )
 }
 
