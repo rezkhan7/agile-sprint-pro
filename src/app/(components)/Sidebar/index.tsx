40d74da4ecdb7a95b4e2ba0de1d38d1c5295c804
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2024-10-04 17:44:40 -0400

    Added links to other pages

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image';
-import { Home, Icon, LockIcon, LucideIcon, X } from 'lucide-react';
+import { Briefcase, Home, Icon, LockIcon, LucideIcon, Search, Settings, User, Users, X } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import Link from 'next/link';
@@ -54,6 +54,11 @@ const Sidebar = () => {
         {/* Navbar Links*/}
             <nav className='z-10 w-full'>
                 <SidebarLink icon= {Home} label="Home" href = '/' />
+                <SidebarLink icon= {Briefcase} label="Timeline" href = '/timeline' />
+                <SidebarLink icon= {Search} label="Search" href = '/search' />
+                <SidebarLink icon= {Settings} label="Settings" href = '/settings' />
+                <SidebarLink icon= {User} label="Users" href = '/users' />
+                <SidebarLink icon= {Users} label="Teams" href = '/teams' />
             </nav>
         </div>  
     </div>
@@ -71,21 +76,16 @@ const SidebarLink = ({
     href,
     icon: Icon,
     label,
-    //isCollapsed
 }: SidebarLinkProps) =>{
     const pathname = usePathname();
     const isActive = pathname === href || (pathname === '/' && href === '/dashboard');
-    const screenWidth = window.innerWidth;
-
-    const dispatch = useAppDispatch();
-    const isSidebarCollapsed = useAppSelector((state)=> state.global.isSidebarCollapsed)
 
     return(
         <Link href={href} className='w-full'>
             <div className={`relative flex cursor-pointer items-center gap-3 transition-colors
              hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
                 isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
-             }`}>
+             } justify-start px-8 py-3`}>
 
                { isActive && (  
                     <div className='absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200'> </div>)
