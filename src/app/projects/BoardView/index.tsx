49727061b497dbc9fc8def781667db8f66a93e8d
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2024-11-08 17:04:21 -0500

    Added button designs to task colums

--- src/app/projects/BoardView/index.tsx
+++ src/app/projects/BoardView/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { DndProvider, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Task as TaskType } from "@/state/api"
+import { EllipsisVertical, Plus } from 'lucide-react';
 
 type BoardProps = {
     id: string;
@@ -87,11 +88,20 @@ const TaskColumn = ({
                 <div className='flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secondary'>
                     <h3 className="flex items-center text-lg font-semibold dark:text-white">
                         {status}{""}
+                        <span className='ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary'
+                        style={{width: "1.5rem", height: "1.5rem"}}>
+                            {taskCount}
+                        </span>
                     </h3>
-                    <span className='ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary'
-                      style={{width: "1.5rem", height: "1.5rem"}}>
-                        {taskCount}
-                    </span>
+                    <div className='flex items-center gap-1'>
+                        <button className='flex h-6 w-5 items-center justify-center dark:text-neutral-500'>
+                            <EllipsisVertical size={26}/>
+                        </button>
+                        <button className='flex h-6 w-6 items-center justify-center rounded bg-gray-200 dark:bg-dark-tertiary dark:text-white'
+                            onClick={()=> setIsModalNewTaskOpen(true)}>
+                                <Plus size={16}/>
+                            </button>
+                    </div>
                 </div>
             </div>
         </div>
