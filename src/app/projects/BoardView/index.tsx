decb554a99ee8fa4b479977521e4898e3d1b1de6
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2024-11-08 16:17:38 -0500

    Added task columns

--- src/app/projects/BoardView/index.tsx
+++ src/app/projects/BoardView/index.tsx
@@ -1,5 +1,8 @@
 import { useGetTasksQuery, useUpdateTaskStatusMutation } from '@/state/api';
 import React from 'react'
+import { DndProvider, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Task as TaskType } from "@/state/api"
 
 type BoardProps = {
     id: string;
@@ -22,8 +25,77 @@ const BoardView = ({ id, setIsModalNewTaskOpen }: BoardProps) => {
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>An error occured while fetching tasks...</div>
   return (
-    <div>index</div>
+    <DndProvider backend={HTML5Backend}>
+        <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4'>
+            {taskStatus.map((status)=>(
+                <TaskColumn
+                  key = {status}
+                  status = {status}
+                  tasks = { tasks || []}
+                  moveTask = {moveTask}
+                  setIsModalNewTaskOpen={setIsModalNewTaskOpen}
+                />
+            ))}
+        </div>
+    </DndProvider>
   )
+} 
+
+type TaskColumnProps = {
+    status:string;
+    tasks: TaskType[];
+    moveTask: (taskId: number, toStatus: string) => void;
+    setIsModalNewTaskOpen: (isOpen: boolean) => void;
+
+
+}
+
+const TaskColumn = ({
+    status,
+    tasks,
+    moveTask,
+    setIsModalNewTaskOpen
+}: TaskColumnProps) =>{
+    const [{isOver}, drop] = useDrop(()=>({
+        accept: "task",
+        drop: (item: {id: number}) => moveTask(item.id, status),
+        collect: (monitor: any) => ({
+            isOver: !!monitor.isOver() // cast as boolean
+        })
+    }))
+
+    const taskCount = tasks.filter((task)=> task.status === status).length;
+    const statusColor: any = {
+        "To Do": "#2563EB",
+        "Work In Progress": "#059669",
+        "Under Review": "#D97706",
+        Completed: "Black"
+    }
+
+    return(
+        <div
+          ref={(instance)=> {
+            drop(instance)
+          }}
+          className={`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ? "bg-blue-100 dark:bg-neutral-950" : ""}`}
+        >
+            <div className='mb-3 flex w-full'>
+                <div
+                    className={`w-2 !bg-[${statusColor[status]}] rounded-s-lg`}
+                    style={{backgroundColor: statusColor[status]}}
+                />
+                <div className='flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secondary'>
+                    <h3 className="flex items-center text-lg font-semibold dark:text-white">
+                        {status}{""}
+                    </h3>
+                    <span className='ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary'
+                      style={{width: "1.5rem", height: "1.5rem"}}>
+                        {taskCount}
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default BoardView;
